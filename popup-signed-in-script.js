9d77d50ea425dbbbdce1217bfa127bc2950f93eb
Saif Lakhani <saiflakhani@gmail.com>
2020-10-27 17:58:12 +0530

    Spotify API Connected

--- popup-signed-in-script.js
+++ popup-signed-in-script.js
@@ -3,3 +3,26 @@ document.querySelector('#sign-out').addEventListener('click', function () {
         if (response.message === 'success') window.close();
     });
 });
+
+chrome.extension.onMessage.addListener(function(message, messageSender, sendResponse) {
+    if(message!=null){
+        if(message['type'] == 'searchResp'){
+            tableResp = '<table><thead><tr><th>Track</th><th>Artist</th></tr></thead><tbody>';
+            for(var i=0;i<message['data']['tracks']['items'].length;i++){
+                tableResp += "<tr><td>" + message['data']['tracks']['items'][i]['name'] +"</td>";
+                tableResp += "<td>" + message['data']['tracks']['items'][i]['artists'][0]['name'] +"</td></tr>";
+            }
+            tableResp += "</tbody></table>"
+            resultsPlaceholder.innerHTML = tableResp;
+        }
+    }
+});
+
+var resultsPlaceholder = document.getElementById('results');
+var templateSource = document.getElementById('results-template').innerHTML;
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log("Searching for songs..."+document.getElementById('query').value)
+    chrome.runtime.sendMessage({message:'search', 'data':document.getElementById('query').value})
+}, false);
