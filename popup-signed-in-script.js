e1348bf3a59eea818b010fff10d0fc19a9db9134
Saif Lakhani <saiflakhani@gmail.com>
2020-10-30 15:15:58 +0530

    Resolved an issue with conflicting bootstrap and internal css; Code Cleanup

--- popup-signed-in-script.js
+++ popup-signed-in-script.js
@@ -20,40 +20,8 @@ chrome.extension.onMessage.addListener(function (message, messageSender, sendRes
     }
 });
 
-
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
     console.log("Searching for songs..." + document.getElementById('query').value)
     chrome.runtime.sendMessage({ message: 'search', 'data': document.getElementById('query').value })
 }, false);
-
-/* Stuff for the animated search bar */
-$(document).ready(function () {
-
-    var $searchTrigger = $('[data-ic-class="search-trigger"]'),
-        $searchInput = $('[data-ic-class="search-input"]'),
-        $searchClear = $('[data-ic-class="search-clear"]');
-
-    $searchTrigger.click(function () {
-        var $this = $('[data-ic-class="search-trigger"]');
-        $this.addClass('active');
-        $searchInput.focus();
-    });
-
-    $searchInput.blur(function () {
-        if ($searchInput.val().length > 0) {
-            return false;
-        } else {
-            $searchTrigger.removeClass('active');
-        }
-    });
-
-    $searchClear.click(function () {
-        $searchInput.val('');
-    });
-
-    $searchInput.focus(function () {
-        $searchTrigger.addClass('active');
-    });
-
-});
