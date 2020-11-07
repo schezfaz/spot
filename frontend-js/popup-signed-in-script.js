14733a5c8d0c4f9efc67db756b7960dd2e84072f
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-11-08 00:58:01 +0530

    cleaned up code

--- frontend-js/popup-signed-in-script.js
+++ frontend-js/popup-signed-in-script.js
@@ -82,24 +82,6 @@ document.querySelector('#sign-out').addEventListener('click', function () {
     });
 });
 
-
-
-// chrome.extension.onMessage.addListener(function (message, messageSender, sendResponse) {
-//     if (message != null) {
-//         if (message['type'] == 'searchResp') {
-//             tableResp = '<table class="table table-dark"><thead><tr><th>Track</th><th>Artist</th></tr></thead><tbody>';
-//             for (var i = 0; i < message['data']['tracks']['items'].length; i++) {
-//                 tableResp += "<tr><td>" + message['data']['tracks']['items'][i]['name'] + "</td>";
-//                 tableResp += "<td>" + message['data']['tracks']['items'][i]['artists'][0]['name'] + "</td></tr>";
-//             }
-//             tableResp += "</tbody></table>"
-//             resultsPlaceholder.innerHTML = tableResp;
-//         }   
-//     }
-
-        
-// });
-
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
     console.log("Searching for songs... " + document.getElementById('query').value)
